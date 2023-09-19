ad5eb563f635ced23b64cf9f45da25b2e97e9b83
JIHYE <jihyem75@gmail.com>
2023-09-19 16:11:46 +0900

    07 Class in Kotlin

--- README.md
+++ README.md
@@ -67,3 +67,19 @@
   ```
 - 함수 파라미터에 기본값을 설정해줄 수 있다
 - 가변인자에는 `vararg` 키워드를 사용하며, 가변인자 함수를 배열과 호출할 때는 `*` 를 붙여주어야 한다
+
+## 9. Kotlin에서 class를 다루는 방법
+- Kotlin 에서는 필드를 만들면 getter 와 (필요에 따라) setter 가 자동으로 생긴다
+  - 때문에 이를 `프로퍼티` 라고 부른다
+  ```kotlin
+  class Person { 
+    val name: String = "jihye"
+    var age: Int = 1
+  }
+  ```
+- Kotlin 에서는 `주생성자` 가 필수이다
+- Kotlin 에서는 constructor 키워드를 사용해 `부생성자` 를 추가로 만들 수 있다
+  - 단, default parameter 나 정적 팩토리 메소드를 추천한다
+- 실제 메모리에 존재하는 것과 무관하게 `custom getter` 와 `custom setter` 를 만들 수 있다
+- custom getter, custom setter 에서 무한루프를 막기위해 `field` 라는 키워드를 사용한다
+  - 이를 `backing field` 라고 부른다
---
+++ src/main/java/com/lecture/lec09/Person.kt
@@ -0,0 +1,78 @@
+package com.lecture.lec09
+
+fun main() {
+    val person = Person("jihye")
+
+    println(person.name)
+    person.age = 20
+
+    println(person.age)
+
+    val javaPerson = JavaPerson("jihye2", 10)
+
+    println(javaPerson.name)
+    javaPerson.age = 30
+
+    println(javaPerson.age)
+
+    Person()
+
+}
+
+class Person(val name: String, var age: Int) {
+
+    init {
+        if(age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다")
+        }
+        println("초기화 불록")
+    }
+    constructor(name: String): this(name, 1) {
+        println("첫 번째 부생성자")
+    }
+    constructor(): this("jihye3") {
+        println("두 번째 부생성자")
+    }
+
+    val isAdult: Boolean
+        get() = this.age >= 20
+}
+
+class Person2(val name: String = "jihye", var age: Int = 1) {
+
+    //    val name = name
+//        get() = field.uppercase()
+
+    fun getUppercaseName():String = this.name.uppercase()
+
+
+    val uppercaseName: String
+        get() = this.name.uppercase()
+
+    init {
+        if(age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다")
+        }
+        println("초기화 불록")
+    }
+
+    val isAdult: Boolean
+        get() = this.age >= 20
+}
+
+class Person3(name: String = "jihye3", var age: Int = 1) {
+
+    var name = name
+        set(value) {
+            field = value.uppercase()
+        }
+    init {
+        if(age <= 0) {
+            throw IllegalArgumentException("나이는 ${age}일 수 없습니다")
+        }
+        println("초기화 불록")
+    }
+
+    val isAdult: Boolean
+        get() = this.age >= 20
+}
